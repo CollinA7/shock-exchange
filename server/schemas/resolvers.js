c460b9f7dd946e057707aa60f2a894d3b075dbb4
cayean <collinayala@gmail.com>
2023-01-12 22:24:16 -0600

    added initial graphql queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,6 +19,59 @@ const resolvers = {
         .select('-__v -password')
         .populate('listings');
     },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('listings');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addListing: async (parent, args, context) => {
+      if (context.user) {
+        const listing = await Listing.create({
+          ...args,
+          username: context.user.username,
+        });
+
+        await Listing.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { listings: listing._id } },
+          { new: true }
+        );
+
+        return listing;
+      }
+
+      throw new AuthenticationError('You need to be logged in');
+    },
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,15 +10,35 @@ const typeDefs = gql`
   type User {
     _id: ID
     username: String
+    firstName: String
+    lastName: String
     email: String
     listings: [Listing]
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     listings(listingText: String): [Listing]
     listing(_id: ID!): Listing
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(
+      username: String!
+      email: String!
+      firstName: String!
+      lastName: String!
+      password: String!
+    ): Auth
+    addListing(listingText: String!): Listing
   }
 `;
 
