31ba8396a322c671a772ebe4f6d522917f07b2fe
cayean <collinayala@gmail.com>
2023-01-14 09:52:39 -0600

    added base user and listing typedefs query's and mutations

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_LISTINGS } from '../utils/queries';
 
 const Home = () => {
-  return <div className="container"></div>;
+  const { loading, data } = useQuery(QUERY_LISTINGS);
+  const listings = data?.listings || [];
+  console.log(listings);
+
+  return (
+    <div className="container">
+      <div className="col-4 mb-3"></div>
+    </div>
+  );
 };
 
 export default Home;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,10 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_LISTINGS = gql`
+  query listings($listingText: String) {
+    listings(listingText: $listingText) {
+      _id
+      listingText
+    }
+  }
+`;
