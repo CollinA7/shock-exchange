5b42de7af97a0b34d0ac24d92b4a6245fa4c2e70
cayean <collinayala@gmail.com>
2023-01-21 15:19:56 -0600

    added single page routing

--- client/src/pages/SingleListing.js
+++ client/src/pages/SingleListing.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_LISTING } from '../utils/queries';
+import { useParams } from 'react-router-dom';
 
 function SingleListing() {
-  return <div></div>;
+  const { id: listingId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_LISTING, {
+    variables: { id: listingId },
+  });
+
+  const listing = data?.listing || {};
+
+  if (loading) {
+    return <div>...Loading...</div>;
+  }
+  return <div>testing</div>;
 }
 
 export default SingleListing;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -10,3 +10,14 @@ export const QUERY_LISTINGS = gql`
     }
   }
 `;
+
+export const QUERY_LISTING = gql`
+  query listing($id: ID!) {
+    listing(_id: $id) {
+      _id
+      listingTitle
+      listingText
+      username
+    }
+  }
+`;
